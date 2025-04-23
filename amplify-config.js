e8f85b96c8df845457bf72c8f47abd6f115cf3cd
TacoSlayah <taco.lover234.gg@gmail.com>
2025-04-23 17:42:47 -0500

    Update amplify-config.js

--- amplify-config.js
+++ amplify-config.js
@@ -1,5 +1,5 @@
 window._config = {
-    userPoolId: 'us-east-1_XXXXXXXXX',         // 🔁 Your Cognito User Pool ID
-    userPoolClientId: 'xxxxxxxxxxxxxxxxxxxx',  // 🔁 Your App Client ID (no secret)
-    region: 'us-east-1'                        // 🔁 Your AWS region
+    userPoolId: 'us-east-1_cK01tos26',         // Your Cognito User Pool ID
+    userPoolClientId: ' 3p5aq0kjpli8plpqgs2r0a4mon',  // Your App Client ID (no secret)
+    region: 'us-east-1'                        // Your AWS region
   };
