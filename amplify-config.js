0a6d58fc28a83d054144cd5e4d10314f6d1ec373
Gallegos <sgg200000@utdallas.edu>
2025-04-24 17:20:19 -0500

    Fixed client ID

--- amplify-config.js
+++ amplify-config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
       userPoolId: 'us-east-1_cK01tos26',             // ✅ Your User Pool ID
-      userPoolClientId: '33p5aq0kjpli8plpqgs2r0a4mon',     // ✅ Your App Client ID (no secret)
+      userPoolClientId: '3p5aq0kjpli8plpqgs2r0a4mon',     // ✅ Your App Client ID (no secret)
       region: 'us-east-1'                             // ✅ Your region
     },
     api: {
