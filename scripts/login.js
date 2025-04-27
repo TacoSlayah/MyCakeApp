38b6ae12c2ae59732d9f43c58dfcbdd68d36f646
TacoSlayah <taco.lover234.gg@gmail.com>
2025-04-27 17:38:06 -0500

    Cake descriptions

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,4 @@
+// Connect to Cognito
 const poolData = {
     UserPoolId: window._config.cognito.userPoolId,
     ClientId: window._config.cognito.userPoolClientId
@@ -5,31 +6,36 @@ const poolData = {
   
   const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
   
+  // Receive login info
   document.getElementById("loginForm").addEventListener("submit", function (e) {
     e.preventDefault();
   
+  //User inputs email and password
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
   
+  // What Cognito receives to check email and password
     const authDetails = new AmazonCognitoIdentity.AuthenticationDetails({
       Username: email,
       Password: password,
     });
   
+    // Creates user in Cognito
     const userData = {
       Username: email,
       Pool: userPool,
     };
   
+    //Check email and password
     const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
   
-    cognitoUser.authenticateUser(authDetails, {
+    cognitoUser.authenticateUser(authDetails, {// if successful
       onSuccess: function (result) {
         alert("Login successful!");
         localStorage.setItem("idToken", result.getIdToken().getJwtToken());
-        window.location.href = "order.html"; // Later you'll create this
+        window.location.href = "order.html";// move to order.html
       },
-      onFailure: function (err) {
+      onFailure: function (err) {// if unsuccessful
         alert(err.message || JSON.stringify(err));
       },
     });
--- scripts/verify.js
+++ scripts/verify.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // Access Cognito
   const poolData = {
     UserPoolId: window._config.cognito.userPoolId,
     ClientId: window._config.cognito.userPoolClientId
@@ -9,22 +10,26 @@ $(document).ready(function () {
   $('#verifyForm').submit(function (e) {
     e.preventDefault();
 
+    // receive user info
     const email = $('#emailInputVerify').val();
     const code = $('#codeInputVerify').val();
 
+    // Add userData in Cognito
     const userData = {
       Username: email,
       Pool: userPool
     };
 
+    // Creates user in Cognito
     const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
+    // Confirms if verification code matches what the user typed in
     cognitoUser.confirmRegistration(code, true, function (err, result) {
       if (err) {
         alert(err.message || JSON.stringify(err));
       } else {
         alert("Verification successful! Redirecting to login...");
-        window.location.href = "login.html";
+        window.location.href = "login.html";// Go to login.html
       }
     });
   });
