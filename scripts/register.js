d2a04a64d7995de488a316463b2c81f834bba809
TacoSlayah <taco.lover234.gg@gmail.com>
2025-04-27 16:01:01 -0500

    Code cleanup- verify.html

--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,5 @@
 $(function () {
+  // Connect Cognito to web app
   const poolData = {
     UserPoolId: window._config.cognito.userPoolId,
     ClientId: window._config.cognito.userPoolClientId
@@ -9,10 +10,11 @@ $(function () {
 
   $("#registerForm").submit(function (e) {
     e.preventDefault();
-
+    // Takes user's email and password that they typed in
     const email = $("#email").val();
     const password = $("#password").val();
-
+    
+    // Stores data in Cognito
     const attributeList = [
       new AmazonCognitoIdentity.CognitoUserAttribute({
         Name: "email",
@@ -20,6 +22,7 @@ $(function () {
       })
     ];
 
+    // Creates account
     userPool.signUp(email, password, attributeList, null, function (err, result) {
       if (err) {
         alert(err.message || JSON.stringify(err));
@@ -27,7 +30,7 @@ $(function () {
       }
 
       alert("Registration successful! Check your email for the verification code.");
-      window.location.href = "verify.html";
+      window.location.href = "verify.html";// success redirects user to verify.html
     });
   });
 });
